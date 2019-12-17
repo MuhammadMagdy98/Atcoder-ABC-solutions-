eb7c6fb2c44570cd0fadce7bc271f16bfe6107f1
MuhammadMagdy98 <52232260+MuhammadMagdy98@users.noreply.github.com>
2019-12-18 00:34:29 +0200

    Update White Cells.cpp

--- White Cells.cpp
+++ White Cells.cpp
@@ -17,25 +17,9 @@ int main() {
     ios::sync_with_stdio(false);
     cin.tie(0);
     cout.tie(0);
-    int h, w, hh, ww;
-    cin >> h >> w >> hh >> ww;
-    int arr[20][20]{};
-    for (int i = 0; i < h && hh--; i++) {
-        for (int j = 0; j < w; j++) {
-            arr[i][j] = 1;
-        }
-    }
-    for (int i = 0; i < w && ww--; i++) {
-        for (int j = 0; j < h; j++) {
-            arr[j][i] = 1;
-        }
-    }
-    int ans = 0;
-    for (int i = 0; i < h; i++) {
-        for (int j = 0; j < w; j++) {
-            if (arr[i][j] == 0) ans++;
-        }
-    }
-    cout << ans << '\n';
+    int rows, cols, r, c;
+    cin >> rows >> cols >> r >> c;
+    int total = cols * r + c * (rows - r);
+    cout << rows * cols - total << '\n';
     return 0;
 }
